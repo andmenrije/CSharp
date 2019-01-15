d30468956a7f0e2688f4c79955304373ef839b70
Andrew Menrije <andmenrije@gmail.com>
2019-01-15 01:44:35 -0800

    Final Version
    Refactored the code so that the function is a single line.

--- SumOfMultiples/SumOfMultiples.cs
+++ SumOfMultiples/SumOfMultiples.cs
@@ -6,8 +6,6 @@ public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        var multipliers = Enumerable.Range(0, max);     
-
-        return multipliers.Where(x => multiples.Any(y => (y > 0) && (x % y == 0))).Sum();
+        return Enumerable.Range(0, max).Where(x => multiples.Any(y => (y > 0) && (x % y == 0))).Sum();
     }
 }
